9ff8e7ead5504d653545d06ccc54152fbb653110
andrew bolek <andrew.bolek@outlook.com>
2019-05-03 17:41:25 -0400

    Simple LinkedList example

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -1,15 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic; //Namespace needed for LinkedList
 
-namespace LinkedList
+public class Program
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        LinkedList<string> months = new LinkedList<string>(); //creating the linkedlist
+        months.AddLast("March"); //last node
+        months.AddFirst("January"); //first node
+
+        var march = months.Find("March"); //assigning node March as var march
+
+        months.AddBefore(march, "February"); //adding to node before march
+        months.AddAfter(march, "April"); //adding node to after march 
+
+        foreach (string month in months) //writing out the linkedlist node by node
         {
+            Console.WriteLine(month);
         }
-    }
+        Console.ReadLine(); // this so far was the example, we can do this again by using seasons.
+
+        LinkedList<string> seasons = new LinkedList<string>();
+        seasons.AddFirst("Spring");
+        seasons.AddLast("Fall");
+
+        var fall = seasons.Find("Fall");
+
+        seasons.AddAfter(fall,"Winter");
+        seasons.AddBefore(fall, "Summer");
+
+        foreach (string season in seasons) //Now we can display all four seasons 
+        {
+            Console.WriteLine(season);
+        }
+        Console.WriteLine("These are your four seasons");
+        Console.ReadLine();
+
+        seasons.Remove(fall); // If for whatever reason you wanted to remove a season, your result would now be
+
+        foreach (string season in seasons)
+        {
+            Console.WriteLine(season);
+        }
+        Console.ReadLine();
+    } 
 }
